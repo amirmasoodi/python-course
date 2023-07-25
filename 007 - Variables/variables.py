8fcbe405842c527bdf3be1860c02cc401801b0b5
Amir Masoodi <128240654+amirmasoodi@users.noreply.github.com>
2023-07-25 18:57:48 +0330

    Create variables.py

---
+++ 007 - Variables/variables.py
@@ -0,0 +1,30 @@
+# How To Define A Variable In Python
+name = "Amir"
+
+# How To Print A Value In Python
+print(name) # OUT : "Amir"
+
+###########################################
+
+# You Cannot Start A Variable Name With NUMBERS
+1name = "Amir"  # Wrong Name
+print(1name)  # OUT : ERROR
+
+# You Cannot Use Special Characters (Except Underline _) In A Variable Name 
+n@ame = "Amir" # Wrong Name
+print(n@me) # OUT : ERROR
+
+# You Cannot Use Spaces Inside A Variable Name
+first name = "Amir" # Wrong Name
+print(first name) # OUT : ERROR
+
+# You Cannot Use Pre-defined Names
+def = "Amir" # Wrong Name
+print(def) # OUT : ERROR
+
+###########################################
+
+# cammelCase Naming
+firstName = "Amir"
+lastName = "Masoodi"
+bankAccountNumber = "1234****0258"
