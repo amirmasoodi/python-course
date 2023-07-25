cc6acb2bb03d4d6184f5bff7ab37d4381fbee448
Amir Masoodi <128240654+amirmasoodi@users.noreply.github.com>
2023-07-25 19:07:18 +0330

    Update numbers.py

--- 008 - Numbers/numbers.py
+++ 008 - Numbers/numbers.py
@@ -1,1 +1,38 @@
-#
+# Definde The Variables
+a = 25
+b = 5
+c = 10
+
+# SUM
+print(25 + 5 + 10) # OUT : 40
+print(a + b + c) # OUT : 40
+
+# SUBTRACT
+print(25 - 5) # OUT : 20
+print(a - b) # OUT : 20
+
+# Multiplication
+print(25 * 5) # OUT : 125
+print(a * b) # OUT : 125
+
+# Division
+print(25 / 5) # OUT : 5
+print(a / b) # OUT : 5
+
+# Modulo
+print(25 % 5) # OUT : 0
+print(a % b) # OUT : 0
+
+print(25 % 10) # OUT : 5
+print(a % c) # OUT : 5
+
+# Power
+print(10 ** 5) # OUT : 100000
+print(c ** b) # OUT : 100000
+
+# CHALLENGE : A Real World Example - Calculating Final Price After Discount
+price = 250000
+discount = 23
+finalPrice = price * (100 - discount) / 100
+print(finalPrice) # OUT : 192500
+
